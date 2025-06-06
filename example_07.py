52ee2d3c750ec31a69d1942cbbce83213413066b
klujic <krunoslav.lujic@predavaci.algebra.hr>
2025-06-06 18:50:16 +0200

    Update example_07.py

--- example_07.py
+++ example_07.py
@@ -1,5 +1,7 @@
+import json
+
 '''
-1. Korak - Potrebno je ucitati podatke iz datoteke movies-250.json i ispisite sadrzaj u konzoli.
+1. Zadatak - Potrebno je ucitati podatke iz datoteke movies-250.json i ispisite sadrzaj u konzoli.
 '''
 
 # datoteka = open('data/movies-250.json', 'r')
@@ -7,16 +9,29 @@
 # print(sadrzaj_datoteke)
 # datoteka.close()
 
-def read_file_content():
-    try:
-        with open('data/movies-250.json', 'r') as file_reader:
-            file_content = file_reader.read()
 
-            print(file_content)
+def read_json_file_content(file_path = 'data/movies-250.json'):
+    try:
+        with open(file_path, 'r') as file_reader:
+            # file_content = file_reader.read()
+            # print(file_content)
+            # return file_reader.read()
+            json_file_content = json.load(file_reader)
+            return json_file_content
 
     except Exception as ex:
         print(f'Dogodila se greska {ex}.')
+        return ''
 
 
 
-read_file_content()
+file_content = read_json_file_content()
+# print(type(file_content))
+# print(file_content)
+
+
+'''
+2. Zadatak
+    Potrenno je kreirati funkciju koja ce iz sadrzaja datoteke ispisati nazive svih filmova
+'''
+
