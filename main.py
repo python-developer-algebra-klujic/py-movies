9e374766adc0a3b5b9feade2aa8bfec0139b5a2e
klujic <krunoslav.lujic@predavaci.algebra.hr>
2025-06-09 18:16:32 +0200

    Update main.py

---
+++ main.py
@@ -0,0 +1,20 @@
+import tkinter as tk
+
+
+# Ucitati sve filmove iz json datoteke
+def load_movies():
+    pass
+
+
+
+
+
+root = tk.Tk()
+root.title('Py Movies')
+
+
+
+
+
+if __name__ == '__main__':
+    root.mainloop()
